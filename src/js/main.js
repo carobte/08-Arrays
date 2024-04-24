a84d9a28fa72660e32f6748e89ef9c82e57098b5
carobte <caro.bustamante.escobar@gmail.com>
2024-04-24 12:14:24 -0500

    agregar

--- src/js/main.js
+++ src/js/main.js
@@ -14,7 +14,10 @@ console.groupCollapsed("Consultar")
 console.log(array)
 console.log(`El array tiene ${array.length} elementos`)
 console.log(`El array en su indice 6: ${array[6]}`)
-console.log(`El array en su último indice: ${array.at(-1)}`) // -> -1 es la última posición, at sólo existe para mostrar
+console.log(`El array en su último indice: ${array.at(-1)}`) // -> -1 es la última posición, at sólo sirve para mostrar
+console.log(`El array incluye Lucas: ${array.includes("Lucas")}`) // -> retorna booleano
+console.log(`El array incluye Lucas: ${array.indexOf("Lucas")}`) // -> retorna la primera posición, si no existe, devuelve -1
+// lastIndexOf = última posición
 
 console.groupEnd()
 
@@ -26,3 +29,13 @@ console.log(array)
 
 console.groupEnd()
 
+console.groupCollapsed("Agregar elementos del array")
+
+array[10] = "bravo" 
+array.push("charlie") // -> al final del array
+array.push("delta")
+array.unshift("1") // -> al inicio del array
+array.unshift("2")
+console.log(array)
+
+console.groupEnd()
