26f20c4c1cbe4771b6bad44fd4c73909afc0dcef
carobte <caro.bustamante.escobar@gmail.com>
2024-04-24 13:05:51 -0500

    aclaración en consultar

--- src/js/main.js
+++ src/js/main.js
@@ -10,13 +10,13 @@ console.groupEnd()
 
 const array = [1, 2, 3, 4, 5, "hola", "mundo", true, false, "alpha"]
 
-console.groupCollapsed("Consultar")
+console.groupCollapsed("Consultar elementos del array")
 console.log(array)
 console.log(`El array tiene ${array.length} elementos`)
 console.log(`El array en su indice 6: ${array[6]}`)
 console.log(`El array en su último indice: ${array.at(-1)}`) // -> -1 es la última posición, at sólo sirve para mostrar
-console.log(`El array incluye Lucas: ${array.includes("Lucas")}`) // -> retorna booleano
-console.log(`El array incluye Lucas: ${array.indexOf("Lucas")}`) // -> retorna la primera posición, si no existe, devuelve -1
+console.log(`El array incluye Lucas (includes): ${array.includes("Lucas")}`) // -> retorna booleano
+console.log(`El array incluye Lucas (indexOf): ${array.indexOf("Lucas")}`) // -> retorna la primera posición, si no existe, devuelve -1
 // lastIndexOf = última posición
 
 console.groupEnd()
